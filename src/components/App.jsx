4b68d0bd43d22d78eb2f3b7769d1f8b7bcd95337
rjgcabrera <rjgcengr@gmail.com>
2018-03-21 17:15:08 -0700

    Render all components and implement click handler on list entries

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,53 @@
-var App = () => (
-  <div>
-    <nav className="navbar">
-      <div className="col-md-6 offset-md-3">
-        <div><h5><em>search</em> view goes here</h5></div>
+class App extends React.Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      videos: window.exampleVideoData,
+      currVideo: 0
+    };
+  }
+  
+  changeCurrentVideo(e, index) {
+    console.log(index);
+    this.setState({
+      currVideo: index
+    });
+  }
+  
+  getVideos(array) {
+    this.setState({
+      videos: array,
+      currVideo: 0
+    });
+  }
+  
+  render() {
+    const changeCurrentVideo = this.changeCurrentVideo.bind(this);
+    const getVideos = this.getVideos.bind(this);
+    return (
+      <div>
+        <nav className="navbar">
+          <div className="col-md-6 offset-md-3">
+            <Search getVideos={getVideos} />
+          </div>
+        </nav>
+        <div className="row">
+          <div className="col-md-7">
+            <VideoPlayer video={this.state.videos[this.state.currVideo]} />
+          </div>
+          <div className="col-md-5">
+            <VideoList videos={this.state.videos} titleClick={changeCurrentVideo}/>
+          </div>
+        </div>
       </div>
-    </nav>
-    <div className="row">
-      <div className="col-md-7">
-        <div><h5><em>videoPlayer</em> view goes here</h5></div>
-      </div>
-      <div className="col-md-5">
-        <div><h5><em>videoList</em> view goes here</h5></div>
-      </div>
-    </div>
-  </div>
-);
+    );    
+  }
+
+}
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
 window.App = App;
+
+ReactDOM.render(<App />, document.getElementById('app'));
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,11 +1,32 @@
-var Search = () => (
-  <div className="search-bar form-inline">
-    <input className="form-control" type="text" />
-    <button className="btn hidden-sm-down">
-      <span className="glyphicon glyphicon-search"></span>
-    </button>
-  </div> 
-);
+class Search extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: ''
+    }
+    
+  }
+  
+  handleSubmit() {
+    window.searchYouTube({}, (data) => {
+      this.props.getVideos(data.items);
+    });
+  }
+  
+  render() {
+    return (
+    <div className="search-bar form-inline">
+      <input className="form-control" type="text" onChange={}/>
+      <button className="btn hidden-sm-down" onClick={}>
+        <span className="glyphicon glyphicon-search"></span>
+      </button>
+    </div>
+    );
+  } 
+  
+}
+
+
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
